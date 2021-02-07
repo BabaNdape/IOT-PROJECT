1593ab52704c73cc35acea411f55f8badc426b2f
Paul Frambot <paul.frambot@telecom-paris.fr>
2021-02-07 20:27:24 +0100

    adapting bdd url for local

--- server/app.js
+++ server/app.js
@@ -189,7 +189,7 @@ app.post('/start-machine', async (req,res) => {
   
 // HTTP SERVER
 app.listen(port, '0.0.0.0', () => {
-  console.log(`HTTP server for dashboard and Raspy at http://localhost:${port}`)
+  console.log(`HTTP server running on port : ${port}`)
 })
 
 // COAP 
@@ -210,7 +210,7 @@ fitiotServer.on('request', function(req, res) {
   fitiotData.save()
       .then(data => {
           res.end(JSON.stringify(payload))
-          console.log("Voici les données qui ont été postées :", payload)
+          console.log("Voici les données qui ont été postées :", payload);
       })
       .catch(err => {
           console.log(err);
@@ -219,5 +219,5 @@ fitiotServer.on('request', function(req, res) {
 })
 
 fitiotServer.listen(function() {
-  console.log('COAP server for fitiot at coap://localhost:5863')
+  console.log('COAP server running on port : 5863');
 })
